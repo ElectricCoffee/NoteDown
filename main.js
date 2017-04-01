5e70a7986f9d720d64f0925ec78381c3703d9c2b
Niko Lepka <slench102+git@gmail.com>
2017-04-01 20:52:16 +0200

    add preliminary files

---
+++ main.js
@@ -0,0 +1,27 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const url  = require('url')
+
+let window
+
+function createWindow() {
+  window = new BrowserWindow({width: 800, height: 600})
+
+  window.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.xhtml'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  window.on('closed', () => {
+    window = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    window = null
+  }
+})
